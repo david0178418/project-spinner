df211db3688258051f02d082011a01314e4328c7
David Granado <davidjgranado@gmail.com>
2020-07-10 00:24:49 -0500

    Fix import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import { createElement } from 'react';
 import { render } from 'react-dom';
 import { App } from './app';
 
 render(
-	React.createElement(App),
+	createElement(App),
 	document.getElementById('app'),
 );
 
