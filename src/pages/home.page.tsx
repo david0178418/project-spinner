5018033c5eb5f64542ac38040753c57a138d6dbd
David Granado <dgranado@glgroup.com>
2020-07-15 14:00:46 -0500

    Adjust styles

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -2,72 +2,12 @@
 import React, { useState, useContext } from 'react';
 import { Wheel } from '@components/wheel';
 import { items } from '@root/data';
-import { PortfolioItem, Orientation } from '@common/interfaces';
+import { Orientation } from '@common/interfaces';
 import clsx from 'clsx';
 import { MainContent } from '@components/main-content';
 import { OrientationContext } from '@common/contexts';
 import { css } from 'linaria';
-
-const layout = css`
-	display: grid;
-	overflow: hidden;
-
-	.left-pane {
-		position: relative;
-	}
-
-	&.landscape {
-		grid-template-columns: auto 50vh;
-		
-		.left-pane {
-			height: 100%;
-		}
-	}
-
-	&.portrait {
-		grid-template-rows: 50vw auto;
-		height: 100vh;
-
-		.left-pane,
-		.right-pane {
-			width: 100%;
-			position: relative;
-		}
-
-		.left-pane {
-			width: 100%;
-		}
-
-		.wheel {
-			bottom: 0;
-			margin: 0 auto;
-			position: absolute;
-		}
-	}
-`;
-
-interface FooProps {
-	item: PortfolioItem;
-}
-
-function Foo(props: FooProps) {
-	const {
-		item,
-	} = props;
-
-	return (
-		(
-			<div className="portfolio-item">
-				{item.wheelImage && (
-					<img className="wheel-logo" src={item.wheelImage}/>
-				)}
-				<div className="wheel-label">
-					{item.label}
-				</div>
-			</div>
-		)
-	);
-}
+import { PortfolioWheelItem } from '@components/portfolio-wheel-item';
 
 export
 function HomePage() {
@@ -101,10 +41,42 @@ function HomePage() {
 					}}
 					onChange={setSelectedItemIndex}
 					itemContent={(item) => (
-						<Foo item={item} />
+						<PortfolioWheelItem item={item} />
 					)}
 				/>
 			</div>
 		</div>
 	);
 }
+
+const layout = css`
+	display: flex;
+	overflow: hidden;
+
+	.left-pane {
+		flex: 1;
+		position: relative;
+	}
+
+	.right-pane {
+	}
+
+	&.landscape {
+		flex-flow: row;
+	}
+
+	&.portrait {
+		flex-flow: column;
+		height: 100vh;
+
+		.left-pane,
+		.right-pane {
+			width: 100%;
+			position: relative;
+		}
+
+		.left-pane {
+			width: 100%;
+		}
+	}
+`;
