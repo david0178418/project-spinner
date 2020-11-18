cb4ffa0f59ccf6632fd4f3f3ce69762872e8cbb5
David Granado <davidjgranado@gmail.com>
2020-11-17 21:45:35 -0600

    Add more projects

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,6 @@
 import { PortfolioItem } from '@common/interfaces';
 
-export
-const items: PortfolioItem[] = [
+const Root: PortfolioItem[] = [
 	{
 		id: 'projects',
 		label: 'Projects',
@@ -12,83 +11,6 @@ const items: PortfolioItem[] = [
 			'root',
 		],
 	},
-		{
-			id: 'projects-apps',
-			label: 'Apps',
-			mainImage: 'https://via.placeholder.com/500x500?text=Node',
-			wheelIcon: 'phone-portrait-sharp',
-			wheelIconColor: '#949494',
-			parentCategories: [
-				'projects',
-			],
-		},
-		{
-			id: 'projects-demos',
-			label: 'Small Demos',
-			mainImage: 'https://via.placeholder.com/500x500?text=Node',
-			wheelIcon: 'bulb-outline',
-			wheelIconColor: '#ffffba',
-			parentCategories: [
-				'projects',
-			],
-		},
-		{
-			id: 'projects-sites',
-			label: 'Sites',
-			mainImage: 'https://via.placeholder.com/500x500?text=Node',
-			wheelIcon: 'globe-outline',
-			wheelIconColor: '#0077be',
-			parentCategories: [
-				'projects',
-			],
-		},
-			{
-				id: 'projects-sites-blog',
-				label: 'Personal Blog',
-				mainImage: 'https://via.placeholder.com/500x500?text=Node',
-				wheelImage: '/images/blog-avatar.png',
-				parentCategories: [
-					'projects-sites',
-				],
-			},
-			{
-				id: 'projects-sites-indiecomicrack',
-				label: 'IndieComic Rack',
-				mainImage: 'https://via.placeholder.com/500x500?text=Node',
-				wheelImage: '/images/icr-logo.jpg',
-				parentCategories: [
-					'projects-sites',
-				],
-			},
-			{
-				id: 'projects-sites-fullviewnews',
-				label: 'FullView News',
-				mainImage: 'https://via.placeholder.com/500x500?text=Node',
-				wheelImage: '/images/fvn-logo.jpg',
-				parentCategories: [
-					'projects-sites',
-				],
-			},
-			{
-				id: 'projects-sites-portfolio',
-				label: 'Portfolio',
-				mainImage: 'https://via.placeholder.com/500x500?text=Node',
-				wheelIcon: 'folder',
-				wheelIconColor: '#f1d592',
-				parentCategories: [
-					'projects-sites',
-				],
-			},
-			{
-				id: 'projects-sites-argdigger',
-				label: 'ArgDigger',
-				mainImage: 'https://via.placeholder.com/500x500?text=Node',
-				wheelIcon: 'bookmarks',
-				wheelIconColor: '#999999',
-				parentCategories: [
-					'projects-sites',
-				],
-			},
 	{
 		id: 'work-history',
 		label: 'Work History',
@@ -115,35 +37,229 @@ const items: PortfolioItem[] = [
 		parentCategories: [
 			'root',
 		],
-	}, {
-		id: 'javascript',
-		label: 'javascript',
-		mainImage: 'https://via.placeholder.com/500x550?text=Javascript',
-		wheelImage: '/images/javascript-logo.png',
+	}, 
+];
+
+const Projects: PortfolioItem[] = [
+	{
+		id: 'projects-all',
+		label: 'All Projects',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'apps',
 		parentCategories: [
-			'typescript',
+			'projects',
 		],
 	}, {
-		id: 'pwa',
-		label: 'pwa',
-		mainImage: 'https://via.placeholder.com/700x550?text=PWA',
-		wheelImage: '/images/pwa-logo.png',
+		id: 'projects-apps',
+		label: 'Apps',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'phone-portrait-sharp',
+		wheelIconColor: '#949494',
 		parentCategories: [
-			'typescript',
+			'projects',
 		],
-	}, {
-		id: 'react',
-		label: 'React',
-		mainImage: 'https://via.placeholder.com/600x600?text=React',
-		wheelImage: '/images/react-logo.webp',
+	},
+	{
+		id: 'projects-demos',
+		label: 'Fun & Games',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'logo-xbox',
 		parentCategories: [
+			'projects',
 		],
-	}, {
-		id: 'angular',
-		label: 'angular',
-		mainImage: 'https://via.placeholder.com/550x550?text=Angular',
-		wheelImage: '/images/angular-logo.png',
+	},
+	{
+		id: 'projects-sites',
+		label: 'Sites',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'globe-outline',
+		wheelIconColor: '#0077be',
 		parentCategories: [
+			'projects',
 		],
 	},
 ];
+
+const ProjectSites: PortfolioItem[] = [
+	{
+		id: 'projects-sites-blog',
+		label: 'Personal Blog',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelImage: '/images/blog-avatar.png',
+		parentCategories: [
+			'projects-sites',
+			'projects-all',
+		],
+	},
+	{
+		id: 'projects-sites-indiecomicrack',
+		label: 'IndieComic Rack',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelImage: '/images/icr-logo.jpg',
+		parentCategories: [
+			'projects-sites',
+			'projects-all',
+		],
+	},
+	{
+		id: 'projects-sites-fullviewnews',
+		label: 'FullView News',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelImage: '/images/fvn-logo.jpg',
+		parentCategories: [
+			'projects-sites',
+			'projects-all',
+		],
+	},
+	{
+		id: 'projects-sites-old-portfolio',
+		label: 'Old Portfolio',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'folder',
+		wheelIconColor: '#999999',
+		parentCategories: [
+			'projects-sites',
+			'projects-all',
+		],
+	},
+	{
+		id: 'projects-sites-portfolio',
+		label: 'Portfolio',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'folder',
+		wheelIconColor: '#f1d592',
+		parentCategories: [
+			'projects-sites',
+			'projects-all',
+		],
+	},
+	{
+		id: 'projects-sites-argdigger',
+		label: 'ArgDigger',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'bookmarks',
+		wheelIconColor: '#4ba7cb',
+		parentCategories: [
+			'projects-sites',
+			'projects-all',
+		],
+	},
+];
+
+const ProjectApps: PortfolioItem[] = [
+	{
+		id: 'projects-apps-shoplystr',
+		label: 'ArgDigger',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelImage: '/images/sl-logo.png',
+		parentCategories: [
+			'projects-apps',
+			'projects-all',
+		],
+	}, {
+		id: 'projects-apps-phungible',
+		label: 'Phungible',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelImage: '/images/p-logo.png',
+		parentCategories: [
+			'projects-apps',
+			'projects-all',
+		],
+	},
+];
+
+const ProjectFunGames: PortfolioItem[] = [
+	{
+		id: 'project-demos-set',
+		label: 'Set',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelImage: '/images/set-card.png',
+		parentCategories: [
+			'projects-demos',
+			'projects-all',
+		],
+	},
+	{
+		id: 'project-demos-meteor',
+		label: 'Meteor',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'game-controller',
+		parentCategories: [
+			'projects-demos',
+			'projects-all',
+		],
+	},
+	{
+		id: 'project-demos-snake',
+		label: 'Snake',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'game-controller',
+		parentCategories: [
+			'projects-demos',
+			'projects-all',
+		],
+	},
+	{
+		id: 'project-demos-Worm',
+		label: 'Worm',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'game-controller',
+		parentCategories: [
+			'projects-demos',
+			'projects-all',
+		],
+	},
+	{
+		id: 'project-demos-rts',
+		label: 'Space RTS',
+		mainImage: 'https://via.placeholder.com/500x500?text=Node',
+		wheelIcon: 'game-controller',
+		parentCategories: [
+			'projects-demos',
+			'projects-all',
+		],
+	},
+];
+
+export
+const items = [
+	...Root,
+	...Projects,
+	...ProjectSites,
+	...ProjectApps,
+	...ProjectFunGames,
+];
+
+// const placeholders = [
+// 	{
+// 		id: 'javascript',
+// 		label: 'javascript',
+// 		mainImage: 'https://via.placeholder.com/500x550?text=Javascript',
+// 		wheelImage: '/images/javascript-logo.png',
+// 		parentCategories: [
+// 			'typescript',
+// 		],
+// 	}, {
+// 		id: 'pwa',
+// 		label: 'pwa',
+// 		mainImage: 'https://via.placeholder.com/700x550?text=PWA',
+// 		wheelImage: '/images/pwa-logo.png',
+// 		parentCategories: [
+// 			'typescript',
+// 		],
+// 	}, {
+// 		id: 'react',
+// 		label: 'React',
+// 		mainImage: 'https://via.placeholder.com/600x600?text=React',
+// 		wheelImage: '/images/react-logo.webp',
+// 		parentCategories: [
+// 		],
+// 	}, {
+// 		id: 'angular',
+// 		label: 'angular',
+// 		mainImage: 'https://via.placeholder.com/550x550?text=Angular',
+// 		wheelImage: '/images/angular-logo.png',
+// 		parentCategories: [
+// 		],
+// 	},
+// ];
